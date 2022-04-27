6c4da72901a1a45f1bc396414359c59ce7c503ea
mefistotelis <mefistotelis@gmail.com>
2022-04-27 10:12:24 +0200

    build: Updated to new autotools
    Also added a new header which is a source of defines for resources.

---
+++ src/swars_ver.h
@@ -0,0 +1,46 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's GeneWars strategy game.
+/******************************************************************************/
+/** @file swars_ver.h
+ *     Project name, version, copyrights and global definitions.
+ * @par Purpose:
+ *     Header file for global names and defines used by resource compiler.
+ * @par Comment:
+ *     Can only contain commands which resource compiler can understand.
+ * @author   Tomasz Lis
+ * @date     10 Oct 2012 - 13 Oct 2021
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef SWARS_VER_H_
+#define SWARS_VER_H_
+
+/* Version definitions */
+#include "config.h"
+//#define VER_MAJOR         1
+//#define VER_MINOR         2
+//#define VER_RELEASE       3
+//#define VER_BUILD         4
+//#define VERSION           "1.2.3.4"
+
+/* Program name, copyrights and file names */
+#define PROGRAM_NAME      "Syndicate Wars Port"
+#define PROGRAM_FULL_NAME "Open-source reimplementation of Syndicate Wars game"
+#define PROGRAM_AUTHORS   "see AUTHORS file"
+#define COMPANY_NAME      "Community based"
+#define INTERNAL_NAME     "swars"
+#define LEGAL_COPYRIGHT   "GNU General Public License"
+#define LEGAL_TRADEMARKS  ""
+#define FILE_VERSION      VERSION
+#define FILE_DESCRIPTION  PROGRAM_NAME
+#define ORIGINAL_FILENAME INTERNAL_NAME".exe"
+#define PRODUCT_NAME      PROGRAM_FULL_NAME
+#define PRODUCT_VERSION   VERSION
+#define DEFAULT_LOG_FILENAME INTERNAL_NAME".log"
+
+#endif // SWARS_VER_H_
+/******************************************************************************/
