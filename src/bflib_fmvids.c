d77b312a99f6ed3c3adf7c92b7a15e6023b05777
mefistotelis <mefistotelis@gmail.com>
2022-05-28 17:34:08 +0200

    swars: Introduced C file for FMV code

---
+++ src/bflib_fmvids.c
@@ -0,0 +1,29 @@
+/******************************************************************************/
+// Bullfrog Engine Emulation Library - for use to remake classic games like
+// Syndicate Wars, Magic Carpet or Dungeon Keeper.
+/******************************************************************************/
+/** @file bflib_fmvids.c
+ *     Full Motion Video playback support.
+ * @par Purpose:
+ *     Implement FMV playback support for games.
+ * @par Comment:
+ *     None.
+ * @author   Tomasz Lis
+ * @date     19 Apr 2022 - 27 May 2022
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include "bflib_fmvids.h"
+
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/******************************************************************************/
+
+/******************************************************************************/
---
+++ src/bflib_fmvids.h
@@ -0,0 +1,35 @@
+/******************************************************************************/
+// Bullfrog Engine Emulation Library - for use to remake classic games like
+// Syndicate Wars, Magic Carpet or Dungeon Keeper.
+/******************************************************************************/
+/** @file bflib_fmvids.h
+ *     Header file for bflib_fmvids.c.
+ * @par Purpose:
+ *     Implement FMV playback support for games.
+ * @par Comment:
+ *     Just a header file - #defines, typedefs, function prototypes etc.
+ * @author   Tomasz Lis
+ * @date     19 Apr 2022 - 27 May 2022
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef BFLIB_FMVIDS_H
+#define BFLIB_FMVIDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/******************************************************************************/
+
+/******************************************************************************/
+
+
+/******************************************************************************/
+#ifdef __cplusplus
+}
+#endif
+#endif
