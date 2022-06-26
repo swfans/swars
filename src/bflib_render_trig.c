f05ff734e8a1121b8275e785835ecfb54d17953a
mefistotelis <mefistotelis@gmail.com>
2022-06-26 19:59:15 +0200

    swars: Remade trig_ll_md01()

--- src/bflib_render_trig.c
+++ src/bflib_render_trig.c
@@ -74,15 +74,20 @@ struct TrigLocals {
     long var_2C; // 0x08
     long var_30; // 0x0C
     long var_34; // 0x10
-    long var_38; // -0x14
+    long var_38; // 0x14
     ulong var_3C; // 0x18
     ulong var_40; // 0x1C
     long var_44; // 0x20
-    ulong var_48[9]; // 0x24 (unkn)
+    ulong var_48[6]; // 0x24 (unkn)
+    long var_60; // 0x3C
+    long var_64; // 0x40
+    ulong var_68; // 0x44 (unkn)
     long var_6C; // 0x48
     ulong var_70[2]; // 0x4C (unkn)
     long var_78; // 0x54
-    ulong var_7C[3]; // 0x58 (unkn)
+    ulong var_7C; // 0x58 (unkn)
+    long var_80; // 0x5C
+    ulong var_84; // 0x60 (unkn)
     ubyte var_88; // 0x64 (unkn)
     ubyte var_89; // 0x65 (unkn)
     ubyte var_8A; // 0x66
@@ -152,7 +157,7 @@ void trig_render_md25(struct TrigLocals *lv);
 void trig_render_md26(struct TrigLocals *lv);
 
 /**
- * overflow flag of subtraction (x-y) of long
+ * whether the subtraction (x-y) of two long ints would overflow
  */
 ubyte __OFSUBL__(long x, long y)
 {
@@ -667,7 +672,7 @@ jump_pr_ll_md00_end:\n \
     if (lv.ll.var_8A)
     {
         TbBool eH_overflow;
-        // overflow flag of addition (lv.ll.var_44+lv.ll.var_78)
+        // whether the addition (lv.ll.var_44+lv.ll.var_78) would overflow
         eH_overflow = __OFSUBL__(lv.ll.var_44, -lv.ll.var_78);
         eH = lv.ll.var_44 + lv.ll.var_78;
         if (((eH < 0) ^ eH_overflow) | (eH == 0))
@@ -715,7 +720,7 @@ jump_pr_ll_md00_end:\n \
                 lv.ll.var_38 = vec_window_height - lv.ll.var_78;
             } else {
                 TbBool eH_overflow;
-                // overflow flag of subtraction (dH - lv.ll.var_38)
+                // whether the subtraction (dH - lv.ll.var_38) would overflow
                 eH_overflow = __OFSUBL__(dH, lv.ll.var_38);
                 eH = dH - lv.ll.var_38;
                 lv.ll.var_8B = ((eH < 0) ^ eH_overflow) | (eH == 0);
@@ -751,6 +756,7 @@ jump_pr_ll_md00_end:\n \
 int trig_ll_md01(struct TrigLocals *lvu, const struct PolyPoint *opt_a,
   const struct PolyPoint *opt_b, const struct PolyPoint *opt_c)
 {
+#if USE_ASM_TRIG_DIVIDED
     int ret;
     asm volatile (" \
             pushal\n \
@@ -907,6 +913,126 @@ jump_pr_ll_md01_end:\n \
                  : "S" (opt_a), "D" (opt_b), "c" (opt_c), "o0" (lv)
                  : "memory", "cc");
     return ret;
+#else
+    struct PolyPoint *pp;
+    long pX, pYa, pYb;
+    long pS;
+
+    lv.ll.var_80 = (lv.ll.var_38 << 16) / lv.ll.var_34;
+    {
+        long dX, wX;
+        long eX;
+        TbBool eX_overflow;
+
+        dX = opt_a->X - opt_c->X;
+        wX = (lv.ll.var_80 * dX) >> 16;
+        dX = opt_b->X - opt_a->X;
+        // whether the addition (wX + dX) would overflow
+        eX_overflow = __OFSUBL__(wX, -dX);
+        eX = wX + dX;
+        if ((eX < 0) ^ eX_overflow)
+            return 0;
+        if (eX != 0) {
+            long long dS, wS;
+            dS = opt_a->S - opt_c->S;
+            wS = (lv.ll.var_80 * dS) >> 16;
+            lv.ll.var_60 = (opt_b->S + wS - opt_a->S) / (eX + 1);
+        }
+    }
+    lv.ll.var_64 = (opt_c->S - opt_a->S) / lv.ll.var_34;
+    pX = opt_a->X << 16;
+    pYa = opt_a->X << 16;
+    pS = opt_a->S;
+    if (lv.ll.var_8A)
+    {
+        long eH;
+        TbBool eH_overflow;
+
+        eH_overflow = __OFSUBL__(lv.ll.var_44, -lv.ll.var_78);
+        eH = lv.ll.var_44 + lv.ll.var_78;
+        if (((eH < 0) ^ eH_overflow) | (eH == 0))
+            return 0;
+        lv.ll.var_44 = eH;
+        lv.ll.var_6C = -lv.ll.var_78;
+        if ( -lv.ll.var_78 - lv.ll.var_38 >= 0 )
+        {
+            lv.ll.var_3C -= lv.ll.var_6C - lv.ll.var_38;
+            lv.ll.var_6C -= lv.ll.var_38;
+            pX += lv.ll.var_28 * lv.ll.var_6C + lv.ll.var_38 * lv.ll.var_28;
+            pYb = lv.ll.var_30 * lv.ll.var_6C + lv.ll.var_40;
+            pS += lv.ll.var_6C * lv.ll.var_64 + lv.ll.var_38 * lv.ll.var_64;
+            if ( lv.ll.var_8C )
+            {
+              lv.ll.var_3C = vec_window_height;
+              lv.ll.var_44 = vec_window_height;
+            }
+            lv.ll.var_38 = 0;
+        }
+        else
+        {
+            lv.ll.var_38 -= lv.ll.var_6C;
+            pX += lv.ll.var_28 * lv.ll.var_6C;
+            pYa += lv.ll.var_6C * lv.ll.var_2C;
+            pS += lv.ll.var_6C * lv.ll.var_64;
+            if (lv.ll.var_8C)
+            {
+                lv.ll.var_44 = vec_window_height;
+                if (lv.ll.var_8B) {
+                    lv.ll.var_38 = vec_window_height;
+                } else {
+                    lv.ll.var_8B = vec_window_height <= lv.ll.var_38;
+                    lv.ll.var_3C = vec_window_height - lv.ll.var_38;
+                }
+            }
+            pYb = lv.ll.var_40;
+        }
+    }
+    else
+    {
+        if (lv.ll.var_8C)
+        {
+            long dH, eH;
+            TbBool eH_overflow;
+
+            dH = vec_window_height - lv.ll.var_78;
+            lv.ll.var_44 = vec_window_height - lv.ll.var_78;
+            if (lv.ll.var_8B) {
+                lv.ll.var_38 = vec_window_height - lv.ll.var_78;
+            } else {
+                eH_overflow = __OFSUBL__(dH, lv.ll.var_38);
+                eH = dH - lv.ll.var_38;
+                lv.ll.var_8B = ((eH < 0) ^ eH_overflow) | (eH == 0);
+                lv.ll.var_3C = eH;
+            }
+        }
+        pYb = lv.ll.var_40;
+    }
+    pp = polyscans;
+    for (; lv.ll.var_38; lv.ll.var_38--)
+    {
+        pp->X = pX;
+        pX += lv.ll.var_28;
+        pp->Y = pYa;
+        pYa += lv.ll.var_2C;
+        pp->S = pS;
+        pS += lv.ll.var_64;
+        ++pp;
+    }
+    if (!lv.ll.var_8B)
+    {
+      for (; lv.ll.var_3C; lv.ll.var_3C--)
+      {
+          pp->X = pX;
+          pX += lv.ll.var_28;
+          pp->Y = pYb;
+          pYb += lv.ll.var_30;
+          pp->S = pS;
+          pS += lv.ll.var_64;
+          ++pp;
+      }
+    }
+    return 1;
+#endif
 }
 
 int trig_ll_md02(struct TrigLocals *lvu, const struct PolyPoint *opt_a,
