68edcbdf6a268b7466e409710d1bf3096c7e3b76
mefistotelis <mefistotelis@gmail.com>
2022-07-04 02:45:00 +0200

    swars: trig() tests: made texture generation
    Genearating a sample texture for testing, using simple xor-based
    algorithm.

--- src/bflib_render.c
+++ src/bflib_render.c
@@ -29,8 +29,11 @@
 #include "bfplanar.h"
 #include "bfanywnd.h"
 #include "bfmouse.h"
+#include "bffile.h"
 #include "bfpng.h"
 #include "bfscrsurf.h"
+#include "bfgentab.h"
+#include "poly.h"
 #include "swlog.h"
 
 
@@ -376,6 +379,159 @@ void make_general_palette(ubyte *pal)
     }
 }
 
+void generate_xor_textute(ubyte *Tex, ushort Width, ushort Height, ushort FillW, ushort FillH, ubyte *Pal, short FactorsX[], short FactorsY[])
+{
+    short x, y;
+    ubyte *t;
+
+    t = Tex;
+    for (y = 0; y < FillH; y++)
+    {
+        for (x = 0; x < FillW; x++)
+        {
+            ubyte r, g, b;
+            r = ((x * FactorsX[0]) >> 10) ^ ((y * FactorsY[0]) >> 10);
+            g = ((x * FactorsX[1]) >> 10) ^ ((y * FactorsY[1]) >> 10);
+            b = ((x * FactorsX[2]) >> 10) ^ ((y * FactorsY[2]) >> 10);
+            t[x] = LbPaletteFindColour(Pal, r, g, b);
+        }
+        t += Width;
+    }
+}
+
+void raw_to_wscreen(short X, short Y, ushort Width, ushort Height, ubyte *Raw)
+{
+    short y;
+    ushort w, h;
+    ubyte *r;
+    ubyte *o;
+
+    r = Raw;
+
+    if (X >= 0) {
+        w = Width;
+    } else if (X > -Width) {
+        w = Width - X;
+        Raw += (-X);
+    } else
+        return;
+    if (X + w > lbDisplay.GraphicsScreenWidth)
+        w = lbDisplay.GraphicsScreenWidth - X;
+
+    if (Y >= 0) {
+        h = Height;
+    } else if (Y > -Height) {
+        h = Height - Y;
+        Raw += lbDisplay.GraphicsScreenWidth * (-Y);
+    } else
+        return;
+    if (Y + h > lbDisplay.GraphicsScreenHeight)
+        h = lbDisplay.GraphicsScreenHeight - Y;
+
+    o = lbDisplay.WScreen + max(Y,0) * lbDisplay.GraphicsScreenWidth + max(X,0);
+
+    for (y = 0; y < h; y++)
+    {
+        memcpy(o, r, w);
+        r += Width;
+        o += lbDisplay.GraphicsScreenWidth;
+    }
+}
+
+TbBool test_trig(void)
+{
+    ubyte pal[PALETTE_8b_SIZE];
+    TbPixel unaffected_colours[] = {0,};
+    ubyte *texmap;
+
+#if 0
+    if (LbErrorLogSetup(NULL, "tst_trig.log", Lb_ERROR_LOG_NEW) != Lb_SUCCESS) {
+        LOGERR("execution log setup failed");
+        return false;
+    }
+#endif
+    if (MockBaseInitialise() != Lb_SUCCESS) {
+        LOGERR("bullfrog Library initialization failed");
+        return false;
+    }
+    LbMemorySetup();
+
+    // Prepare a palette, and colour tables for it
+    make_general_palette(pal);
+    LbFileSaveAt("tst_gp.pal", &pal, sizeof(pal));
+    LbColourTablesGenerate(pal, unaffected_colours, "tst_gptbl.dat");
+
+    if (MockScreenSetupAnyMode(Lb_SCREEN_MODE_640_480_8, 640, 480, pal) != Lb_SUCCESS) {
+        LOGERR("bullfrog Library initialization failed");
+        return false;
+    }
+
+    MockScreenLock();
+
+    texmap = LbMemoryAlloc(256*256*1);
+    {
+        ubyte *tmap;
+        short factors0[] = {256, 256, 256};
+        tmap = texmap;
+        generate_xor_textute(tmap, 256, 256, 256,  64, pal, factors0, factors0);
+        short factors1[] = {512,   0,   0};
+        tmap = texmap + 256 *  64 + 0;
+        generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors1, factors1);
+        short factors2[] = {  0, 512,   0};
+        tmap = texmap + 256 *  64 + 128;
+        generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors2, factors2);
+        short factors3[] = {  0,   0, 512};
+        tmap = texmap + 256 * 128 + 0;
+        generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors3, factors3);
+        short factors4[] = {512, 512,   0};
+        tmap = texmap + 256 * 128 + 128;
+        generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors4, factors4);
+        short factors5[] = {512,   0, 512};
+        tmap = texmap + 256 * 192 + 0;
+        generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors5, factors5);
+        short factors6[] = {  0, 512, 512};
+        tmap = texmap + 256 * 192 + 128;
+        generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors6, factors6);
+    }
+
+    setup_vecs(lbDisplay.WScreen, texmap, lbDisplay.PhysicalScreenWidth,
+        lbDisplay.PhysicalScreenWidth, lbDisplay.PhysicalScreenHeight);
+
+#if 0
+    LbPaletteDraw(320-256-8, 32, 16, 16, 0x01);
+    raw_to_wscreen(320+8, 32, 256, 256, texmap);
+#endif
+
+    {
+        struct PolyPoint point_a, point_b, point_c;
+
+        vec_mode = RendVec_mode00;//RendVec_mode17
+        vec_colour = LbPaletteFindColour(pal, 128 >> 2, 128 >> 2, 0 >> 2);
+        point_a.S = 0x200000;
+        point_b.S = 0x8000;
+        point_c.S = 0x200000;
+        point_a.X = 319;
+        point_a.Y = 269;
+        point_b.X = 340;
+        point_b.Y = 329;
+        point_c.X = 325;
+        point_c.Y = 329;
+        if ((point_c.Y - point_b.Y) * (point_b.X - point_a.X) - (point_b.Y - point_a.Y) * (point_c.X - point_b.X) > 0)
+            trig(&point_a, &point_b, &point_c);
+        else
+            trig(&point_a, &point_c, &point_b);
+    }
+
+    LbMemoryFree(texmap);
+    LbPngSave("tst_trig.png", lbDisplay.WScreen, pal, true);
+    
+    MockScreenUnlock();
+    MockScreenReset();
+    LbMemoryReset();
+    LOGSYNC("passed");
+    return true;
+}
+
 TbBool test_memory(void)
 {
     void *p;
@@ -394,34 +550,4 @@ TbBool test_memory(void)
     return true;
 }
 
-TbBool test_trig(void)
-{
-    ubyte pal[PALETTE_8b_SIZE];
-
-#if 0
-    if (LbErrorLogSetup(NULL, "tst_trig.log", Lb_ERROR_LOG_NEW) != Lb_SUCCESS) {
-        LOGERR("execution log setup failed");
-        return false;
-    }
-#endif
-    if (MockBaseInitialise() != Lb_SUCCESS) {
-        LOGERR("bullfrog Library initialization failed");
-        return false;
-    }
-    LbMemorySetup();
-    make_general_palette(pal);
-    if (MockScreenSetupAnyMode(Lb_SCREEN_MODE_640_480_8, 640, 480, pal) != Lb_SUCCESS) {
-        LOGERR("bullfrog Library initialization failed");
-        return false;
-    }
-    MockScreenLock();
-
-    LbPngSave("tst_trig.png", lbDisplay.WScreen, pal, true);
-    
-    MockScreenUnlock();
-    MockScreenReset();
-    LbMemoryReset();
-    LOGSYNC("passed");
-    return true;
-}
 /******************************************************************************/
