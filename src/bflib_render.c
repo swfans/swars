573119648cb1e50e96e9575fe5b5ee6c59b41e16
mefistotelis <mefistotelis@gmail.com>
2022-07-29 02:43:03 +0200

    swars: trig() test no 7 enabled

--- src/bflib_render.c
+++ src/bflib_render.c
@@ -568,7 +568,7 @@ void test_trig_draw_random_triangles(const ubyte *pal)
 TbBool test_trig(void)
 {
     static ulong seeds[] = {0x0, 0xD15C1234, 0xD15C0000, 0xD15C0005, 0xD15C000F, 0xD15C03DC,
-      0xD15C07DF, };
+      0xD15C07DF, 0xD15CE896, };
     ubyte pal[PALETTE_8b_SIZE];
     ubyte ref_pal[PALETTE_8b_SIZE];
     TbPixel unaffected_colours[] = {0,};
