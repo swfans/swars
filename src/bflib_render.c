d8ac7ab2eb49f10ecb73e4181e518b45d5bf3c70
mefistotelis <mefistotelis@gmail.com>
2022-08-01 21:09:33 +0200

    swars: Remove routines for tests now in bflibrary

--- src/bflib_render.c
+++ src/bflib_render.c
@@ -42,98 +42,13 @@
 #include "../bflibrary/tests/mock_palette.c"
 #include "../bflibrary/tests/mock_screen.c"
 #include "../bflibrary/tests/mock_windows.c"
+#include "../bflibrary/tests/helpers_screen.c"
 
 /******************************************************************************/
 extern ulong seed;
 /******************************************************************************/
 
 ulong LbRandomAnyShort(void);
-
-void generate_xor_textute(ubyte *Tex, ushort Width, ushort Height, ushort FillW,
-  ushort FillH, const ubyte *Pal, short FactorsX[], short FactorsY[])
-{
-    short x, y;
-    ubyte *t;
-
-    t = Tex;
-    for (y = 0; y < FillH; y++)
-    {
-        for (x = 0; x < FillW; x++)
-        {
-            ubyte r, g, b;
-            r = ((x * FactorsX[0]) >> 10) ^ ((y * FactorsY[0]) >> 10);
-            g = ((x * FactorsX[1]) >> 10) ^ ((y * FactorsY[1]) >> 10);
-            b = ((x * FactorsX[2]) >> 10) ^ ((y * FactorsY[2]) >> 10);
-            t[x] = LbPaletteFindColour(Pal, r, g, b);
-        }
-        t += Width;
-    }
-}
-
-void raw_to_wscreen(short X, short Y, ushort Width, ushort Height, ubyte *Raw)
-{
-    short y;
-    ushort w, h;
-    ubyte *r;
-    ubyte *o;
-
-    r = Raw;
-
-    if (X >= 0) {
-        w = Width;
-    } else if (X > -Width) {
-        w = Width - X;
-        Raw += (-X);
-    } else
-        return;
-    if (X + w > lbDisplay.GraphicsScreenWidth)
-        w = lbDisplay.GraphicsScreenWidth - X;
-
-    if (Y >= 0) {
-        h = Height;
-    } else if (Y > -Height) {
-        h = Height - Y;
-        Raw += lbDisplay.GraphicsScreenWidth * (-Y);
-    } else
-        return;
-    if (Y + h > lbDisplay.GraphicsScreenHeight)
-        h = lbDisplay.GraphicsScreenHeight - Y;
-
-    o = lbDisplay.WScreen + max(Y,0) * lbDisplay.GraphicsScreenWidth + max(X,0);
-
-    for (y = 0; y < h; y++)
-    {
-        memcpy(o, r, w);
-        r += Width;
-        o += lbDisplay.GraphicsScreenWidth;
-    }
-}
-
-void generate_example_texture_map_xor_based(const ubyte *pal, ubyte *texmap)
-{
-    ubyte *tmap;
-    short factors0[] = {256, 256, 256};
-    tmap = texmap;
-    generate_xor_textute(tmap, 256, 256, 256,  64, pal, factors0, factors0);
-    short factors1[] = {512,   0,   0};
-    tmap = texmap + 256 *  64 + 0;
-    generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors1, factors1);
-    short factors2[] = {  0, 512,   0};
-    tmap = texmap + 256 *  64 + 128;
-    generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors2, factors2);
-    short factors3[] = {  0,   0, 512};
-    tmap = texmap + 256 * 128 + 0;
-    generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors3, factors3);
-    short factors4[] = {512, 512,   0};
-    tmap = texmap + 256 * 128 + 128;
-    generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors4, factors4);
-    short factors5[] = {512,   0, 512};
-    tmap = texmap + 256 * 192 + 0;
-    generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors5, factors5);
-    short factors6[] = {  0, 512, 512};
-    tmap = texmap + 256 * 192 + 128;
-    generate_xor_textute(tmap, 256, 256, 128,  64, pal, factors6, factors6);
-}
 
 void test_trig_draw_random_triangles(const ubyte *pal)
 {
