fee9b3b255480f98552822f954fd140f89c7c93a
mefistotelis <mefistotelis@gmail.com>
2022-09-23 12:48:45 +0200

    swars: Remade FreeAwe32Soundfont()

--- src/bflib_snd_sys.c
+++ src/bflib_snd_sys.c
@@ -17,8 +17,35 @@
  *     (at your option) any later version.
  */
 /******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 #include "bflib_snd_sys.h"
+
+#include "bfmemory.h"
+#include "awe32.h"
+#include "dpmi.h"
+#include "snderr.h"
+#include "msssys.h"
+#include "sound.h"
 /******************************************************************************/
+extern char SoundProgressMessage[256];
+extern TbBool MusicAble;
+extern TbBool AILStartupAlreadyInitiated;
+extern TbBool UseCurrentAwe32Soundfont;
+extern TbBool MusicInstalled;
+extern AudioDriverChoice MusicInstallChoice;
+extern char FullMDI_INIPath[144];
+extern char SoundDataPath[144];
+extern char MusicType[6];
+extern TbBool AutoScanForSoundHardware;
+extern MDI_DRIVER *MusicDriver;
+extern uint8_t *awe_buffer;
+extern uint16_t awe_buffer_seg;
+extern uint8_t *awe_preset;
+extern uint16_t awe_preset_seg;
+extern TbBool Awe32SoundfontLoaded;
+
 void StopAllSamples(void)
 {
     asm volatile ("call ASM_StopAllSamples\n"
@@ -61,6 +88,25 @@ void InitSound(void)
 {
     asm volatile ("call ASM_InitSound\n"
         :  :  : "eax" );
+}
+
+void LoadAwe32Soundfont(const char *str)
+{
+    asm volatile ("call ASM_LoadAwe32Soundfont\n"
+        :  :  "a" (str) );
+}
+
+void FreeAwe32Soundfont(void)
+{
+    if (!UseCurrentAwe32Soundfont && MusicInstalled  && MusicAble
+      && !strcasecmp(MusicInstallChoice.str2, "SBAWE32.MDI")
+      && !strcasecmp(MusicType, "w") )
+    {
+        AWEFreeMem(MusicDriver, 1);
+        FreeDOSmem(awe_buffer, awe_buffer_seg);
+        FreeDOSmem(awe_preset, awe_preset_seg);
+        Awe32SoundfontLoaded = 0;
+    }
 }
 
 void InitMusic(void)
