f875dafe4dcaa1217b114c4e1bbee62427964c3e
mefistotelis <mefistotelis@gmail.com>
2023-06-14 03:59:19 +0200

    bflibrary: Added INI parser
    The original library had no utility functions to parse INI files, and
    each game implemented a simple one function parser when neccessary.
    But the simple implementation lead to issues as it could not recognize
    many of standard features of INI format (causing bad parse if a user
    modified the file by hand), and was a bit different for each game,
    causing confusion. This set of simple functions solves these problems.

---
+++ bflibrary/include/bfini.h
@@ -0,0 +1,142 @@
+/******************************************************************************/
+// Bullfrog Engine Emulation Library - for use to remake classic games like
+// Syndicate Wars, Magic Carpet, Genewars or Dungeon Keeper.
+/******************************************************************************/
+/** @file bfini.h
+ *     Header file for gini.c.
+ * @par Purpose:
+ *     Implementation of INI File Format parser.
+ * @par Comment:
+ *     Allows reading INI initialization configuration files.
+ * @author   Tomasz Lis
+ * @date     30 Jan 2009 - 05 Jun 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef BFLIBRARY_BFINI_H_
+#define BFLIBRARY_BFINI_H_
+
+#include "bftypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma pack(1)
+
+/** Named enumeration item, used for INI file parsing. */
+struct TbNamedEnum {
+    const char *name;
+    int num;
+};
+
+/** State of an INI buffer parsing. */
+struct TbIniParser {
+    /** Current position of parsing within the buf. */
+    long pos;
+    /** Line number, used when loading text files. */
+    ulong line_num;
+    /** Parsed input string buffer. */
+    const char *buf;
+    /** Parsed input string buffer length. */
+    long buflen;
+};
+
+#pragma pack()
+
+/**
+ * Prepares state structure for parsing INI buffer.
+ *
+ * @param parser The parser state structure to initialize.
+ * @param buf The input string buffer.
+ * @param buflen Length of the input string buffer.
+ * @return Gives Lb_SUCCESS on proper initialization.
+ */
+TbResult LbIniParseStart(struct TbIniParser *parser, const char *buf, long buflen);
+
+/**
+ * Marks end of use of state structure for parsing INI buffer.
+ *
+ * @param parser The parser state structure to finalize.
+ * @return Gives Lb_SUCCESS on proper finalization.
+ */
+TbResult LbIniParseEnd(struct TbIniParser *parser);
+
+/**
+ * Parses INI buffer, skipping current position to after next EOLN.
+ *
+ * @param parser The parser state defining input buffer and current position.
+ * @return Gives True if there is unparsed data remaining in the input buffer, false if the end was reached.
+ */
+TbBool LbIniSkipToNextLine(struct TbIniParser *parser);
+
+/**
+ * Searches for start of INI file section with given name.
+ *
+ * @param parser The parser state defining input buffer and current position.
+ * @param sectname String name of the section to search for.
+ * @return Returns Lb_SUCCESS if the block is found, Lb_FAIL if buffer exceeded.
+ */
+TbResult LbIniFindSection(struct TbIniParser *parser, const char *sectname);
+
+/**
+ * Parses INI buffer, recognizing a key from given list.
+ *
+ * @param parser The parser state defining input buffer and current position.
+ * @param keylist List of expected INI keys in current section.
+ * @return If positive integer is returned, it is the command number recognized in the line.
+ * If 0 is returned, that means the current line did not contained any key-value pair and should be skipped.
+ * If -1 is returned, that means we have reached end of file.
+ * If -2 is returned, that means the key wasn't recognized.
+ * If -3 is returned, that means we have reached end of the INI section.
+ */
+int LbIniRecognizeKey(struct TbIniParser *parser, const struct TbNamedEnum keylist[]);
+
+/**
+ * Parses INI value and fills destination buffer with the whole string, that is until EOLN.
+ *
+ * @param parser The parser state defining input buffer and current position.
+ * @param dst The destination string buffer.
+ * @param dstlen Length of the destination string buffer.
+ * @return Gives length of the string written to dst, or 0 if nothing got written.
+ */
+int LbIniValueGetStrWhole(struct TbIniParser *parser, char *dst, long dstlen);
+
+/**
+ * Parses INI value and fills destination buffer with the next word string.
+ *
+ * @param parser The parser state defining input buffer and current position.
+ * @param dst The destination string buffer.
+ * @param dstlen Length of the destination string buffer.
+ * @return Gives length of the string written to dst, or 0 if nothing got written.
+ */
+int LbIniValueGetStrWord(struct TbIniParser *parser, char *dst, long dstlen);
+
+/**
+ * Parses INI value and if it stores enum item name, returns the enumeration item number.
+ *
+ * @param parser The parser state defining input buffer and current position.
+ * @param vallist Array of named enumerations, ended with empty entry.
+ * @return Gives enumeration item number, or 0 if not found.
+ */
+int LbIniValueGetNamedEnum(struct TbIniParser *parser, const struct TbNamedEnum vallist[]);
+
+/**
+ * Get name of enum member with given number.
+ *
+ * @param nelist Array of named enumerations, ended with empty entry.
+ * @param num Enumeration item number.
+ * @return Gives string with name of enum member, or empty string.
+ */
+const char *LbNamedEnumGetName(const struct TbNamedEnum nelist[], int num);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif // BFLIBRARY_BFINI_H_
+/******************************************************************************/
---
+++ bflibrary/src/general/gini.c
@@ -0,0 +1,343 @@
+/******************************************************************************/
+// Bullfrog Engine Emulation Library - for use to remake classic games like
+// Syndicate Wars, Magic Carpet, Genewars or Dungeon Keeper.
+/******************************************************************************/
+/** @file gini.c
+ *     Implementation of INI File Format parser.
+ * @par Purpose:
+ *     Read INI initialization configuration files.
+ * @par Comment:
+ *     The original library had no utility functions to parse INI files, and
+ *     each game implemented a simple one function parser when neccessary.
+ *     But the simple implementation lead to issues as it could not recognize
+ *     many of standard features of INI format (causing bad parse if a user
+ *     modified the file by hand), and was a bit different for each game,
+ *     causing confusion. This set of simple functions solves these problems.
+ * @author   Tomasz Lis
+ * @date     30 Jan 2009 - 05 Jun 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include <assert.h>
+#include <string.h>
+#include "bfini.h"
+
+#include "bftypes.h"
+#include "bfstrut.h"
+#include "privbflog.h"
+
+TbResult LbIniParseStart(struct TbIniParser *parser, const char *buf, long buflen)
+{
+    parser->pos = 0;
+    parser->line_num = 1;
+    parser->buf = buf;
+    parser->buflen = buflen;
+    return Lb_SUCCESS;
+}
+
+TbResult LbIniParseEnd(struct TbIniParser *parser)
+{
+    parser->pos = parser->buflen;
+    return Lb_SUCCESS;
+}
+
+TbBool LbIniSkipToNextLine(struct TbIniParser *parser)
+{
+    // Skip to end of the line
+    while (parser->pos < parser->buflen)
+    {
+        if ((parser->buf[parser->pos] == '\r') ||
+            (parser->buf[parser->pos] == '\n'))
+            break;
+        parser->pos++;
+    }
+    // Go to start of next line
+    while (parser->pos < parser->buflen)
+    {
+        if ((uchar)parser->buf[parser->pos] > 32)
+            break;
+        if (parser->buf[parser->pos] == '\n')
+            parser->line_num++;
+        parser->pos++;
+    }
+    return (parser->pos < parser->buflen);
+}
+
+static TbBool IniSkipSpaces(struct TbIniParser *parser)
+{
+    while (parser->pos < parser->buflen)
+    {
+        if ((parser->buf[parser->pos] != ' ') &&
+            (parser->buf[parser->pos] != '\t') &&
+            (parser->buf[parser->pos] != 26) &&
+            ((uchar)parser->buf[parser->pos] >= 7))
+            break;
+        parser->pos++;
+    }
+    return (parser->pos < parser->buflen);
+}
+
+TbResult LbIniFindSection(struct TbIniParser *parser, const char *sectname)
+{
+    int blname_len;
+    parser->pos = 0;
+    parser->line_num = 1;
+    blname_len = strlen(sectname);
+    while (parser->pos + blname_len + 2 < parser->buflen)
+    {
+        // Skipping starting spaces
+        if (!IniSkipSpaces(parser))
+            break;
+        // Checking if this line is start of a block
+        if (parser->buf[parser->pos] != '[')
+        {
+            LbIniSkipToNextLine(parser);
+            continue;
+        }
+        parser->pos++;
+        // Skipping any spaces
+        if (!IniSkipSpaces(parser))
+            break;
+        if (parser->pos + blname_len + 2 >= parser->buflen)
+            break;
+        if (strncasecmp(&parser->buf[parser->pos], sectname, blname_len) != 0)
+        {
+            LbIniSkipToNextLine(parser);
+            continue;
+        }
+        parser->pos += blname_len;
+        // Skipping any spaces
+        if (!IniSkipSpaces(parser))
+            break;
+        if (parser->buf[parser->pos] != ']')
+        {
+            LbIniSkipToNextLine(parser);
+            continue;
+        }
+        LbIniSkipToNextLine(parser);
+        return Lb_SUCCESS;
+    }
+    return Lb_FAIL;
+}
+
+int LbIniRecognizeKey(struct TbIniParser *parser, const struct TbNamedEnum keylist[])
+{
+    int i,cmdname_len;
+    LOGNO("Starting");
+    if ((parser->pos) >= parser->buflen)
+        return -1;
+    // Skipping starting spaces
+    while ((parser->buf[parser->pos] == ' ') ||
+           (parser->buf[parser->pos] == '\t') ||
+           (parser->buf[parser->pos] == '\n') ||
+           (parser->buf[parser->pos] == '\r') ||
+           (parser->buf[parser->pos] == 26) ||
+           ((uchar)parser->buf[parser->pos] < 7))
+    {
+        parser->pos++;
+        if (parser->pos >= parser->buflen) return -1;
+    }
+    // Checking if this line is a comment
+    if (parser->buf[parser->pos] == ';')
+        return 0;
+    // Checking if this line is start of a block
+    if (parser->buf[parser->pos] == '[')
+        return -3;
+    // Finding command number
+    i = 0;
+    while (keylist[i].num > 0)
+    {
+        cmdname_len = strlen(keylist[i].name);
+        if (parser->pos + cmdname_len > parser->buflen) {
+            i++;
+            continue;
+        }
+        // Find a matching command
+        if (strncasecmp(&parser->buf[parser->pos], keylist[i].name, cmdname_len) == 0)
+        {
+            parser->pos += cmdname_len;
+            // if we're not at end of input buffer..
+            if (parser->pos < parser->buflen)
+            {
+                // make sure it's whole command, not just start of different one
+               if ((parser->buf[parser->pos] != ' ') &&
+                   (parser->buf[parser->pos] != '\t') &&
+                   (parser->buf[parser->pos] != '=')  &&
+                   ((uchar)parser->buf[parser->pos] >= 7))
+               {
+                  parser->pos -= cmdname_len;
+                  i++;
+                  continue;
+               }
+               // Skipping spaces between command and parameters
+               while ((parser->buf[parser->pos] == ' ') ||
+                  (parser->buf[parser->pos] == '\t') ||
+                  (parser->buf[parser->pos] == '=')  ||
+                  ((uchar)parser->buf[parser->pos] < 7))
+               {
+                 parser->pos++;
+                 if (parser->pos >= parser->buflen) break;
+               }
+            }
+            return keylist[i].num;
+        }
+        i++;
+    }
+    return -2;
+}
+
+int LbIniValueGetStrWhole(struct TbIniParser *parser, char *dst, long dstlen)
+{
+    int i;
+    TbBool in_quotes;
+    in_quotes = false;
+    if (parser->pos >= parser->buflen) return 0;
+    // Skipping spaces after previous parameter
+    while ((parser->buf[parser->pos] == ' ') || (parser->buf[parser->pos] == '\t'))
+    {
+        parser->pos++;
+        if (parser->pos >= parser->buflen) return 0;
+    }
+    if (parser->buf[parser->pos] == '\"') {
+        in_quotes = true;
+        parser->pos++;
+        if (parser->pos >= parser->buflen) return 0;
+    }
+    for (i = 0; i + 1 < dstlen; i++)
+    {
+        if (in_quotes) {
+            if ((parser->buf[parser->pos] == '\"') ||
+                 (parser->buf[parser->pos] == '\r') ||
+                 (parser->buf[parser->pos] == '\n'))
+                break;
+        } else {
+            if ((parser->buf[parser->pos] == '\r') ||
+                (parser->buf[parser->pos] == '\n') ||
+                ((uchar)parser->buf[parser->pos] < 7))
+              break;
+        }
+        dst[i] = parser->buf[parser->pos];
+        parser->pos++;
+        if (parser->pos >= parser->buflen) break;
+    }
+    dst[i] = '\0';
+    return i;
+}
+
+int LbIniValueGetStrWord(struct TbIniParser *parser, char *dst, long dstlen)
+{
+    int i;
+    TbBool in_quotes;
+    in_quotes = false;
+    if (parser->pos >= parser->buflen) return 0;
+    // Skipping spaces after previous parameter
+    while ((parser->buf[parser->pos] == ' ') || (parser->buf[parser->pos] == '\t'))
+    {
+        parser->pos++;
+        if (parser->pos >= parser->buflen) return 0;
+    }
+    if (parser->buf[parser->pos] == '\"') {
+        in_quotes = true;
+        parser->pos++;
+        if (parser->pos >= parser->buflen) return 0;
+    }
+    for (i=0; i+1 < dstlen; i++)
+    {
+        if (in_quotes) {
+            if ((parser->buf[parser->pos] == '\"') ||
+                 (parser->buf[parser->pos] == '\r') ||
+                 (parser->buf[parser->pos] == '\n'))
+                break;
+        } else {
+            if ((parser->buf[parser->pos] == ' ') ||
+                 (parser->buf[parser->pos] == '\t') ||
+                 (parser->buf[parser->pos] == '\r') ||
+                 (parser->buf[parser->pos] == '\n') ||
+                 ((uchar)parser->buf[parser->pos] < 7))
+                break;
+        }
+        dst[i] = parser->buf[parser->pos];
+        parser->pos++;
+        if (parser->pos >= parser->buflen) {
+            i++;
+            break;
+        }
+    }
+    dst[i] = '\0';
+    return i;
+}
+
+int LbIniValueGetNamedEnum(struct TbIniParser *parser, const struct TbNamedEnum vallist[])
+{
+    int i;
+    int par_len;
+    TbBool in_quotes;
+    in_quotes = false;
+    if (parser->pos >= parser->buflen) return 0;
+    // Skipping spaces after previous parameter
+    while ((parser->buf[parser->pos] == ' ') ||
+           (parser->buf[parser->pos] == '\t'))
+    {
+        parser->pos++;
+        if (parser->pos >= parser->buflen) return 0;
+    }
+    if (parser->buf[parser->pos] == '\"') {
+        in_quotes = true;
+        parser->pos++;
+        if (parser->pos >= parser->buflen) return 0;
+    }
+    i = 0;
+    while (vallist[i].name != NULL)
+    {
+        par_len = strlen(vallist[i].name);
+        if (strncasecmp(&parser->buf[parser->pos], vallist[i].name, par_len) == 0)
+        {
+            // If EOLN found, finish and return position before the EOLN
+            if ((parser->buf[parser->pos + par_len] == '\n') ||
+                (parser->buf[parser->pos + par_len] == '\r'))
+            {
+              parser->pos += par_len;
+              return vallist[i].num;
+            }
+            if (in_quotes) {
+                // If quote ends, finish and return position after the quote
+                if (parser->buf[parser->pos + par_len] == '\"')
+                {
+                    parser->pos += par_len + 1;
+                    return vallist[i].num;
+                }
+            } else {
+                // If non-EOLN blank char, finish and return position after the char
+                if ((parser->buf[parser->pos + par_len] == ' ') ||
+                    (parser->buf[parser->pos + par_len] == '\t') ||
+                    ((uchar)parser->buf[parser->pos + par_len] < 7))
+                {
+                    parser->pos += par_len + 1;
+                    return vallist[i].num;
+                }
+            }
+        }
+        i++;
+    }
+    return 0;
+}
+
+const char *LbNamedEnumGetName(const struct TbNamedEnum nelist[], int num)
+{
+    int i;
+    i = 0;
+    while (nelist[i].name != NULL)
+    {
+        LOGNO("\"%s\", %d %d", nelist[i].name, nelist[i].num, num);
+        if (nelist[i].num == num)
+            return nelist[i].name;
+        i++;
+    }
+    return lbEmptyString;
+}
+
+/******************************************************************************/
