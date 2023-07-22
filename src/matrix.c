766f12885d52db9ebbcea2e1ea578b9d857f200a
mefistotelis <mefistotelis@gmail.com>
2023-07-22 10:49:18 +0200

    swars: Added file for matrix operations implementation

---
+++ src/matrix.c
@@ -0,0 +1,23 @@
+/******************************************************************************/
+// Syndicate Wars Port, source port of the classic strategy game from Bullfrog.
+/******************************************************************************/
+/** @file matrix.c
+ *     Routines implementing matrix support.
+ * @par Purpose:
+ *     Implement functions for handling multi-dimensional matrices.
+ * @par Comment:
+ *     None.
+ * @author   Tomasz Lis
+ * @date     27 May 2022 - 12 Jun 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include "matrix.h"
+#include "swlog.h"
+/******************************************************************************/
+
+/******************************************************************************/
---
+++ src/matrix.h
@@ -0,0 +1,41 @@
+/******************************************************************************/
+// Syndicate Wars Port, source port of the classic strategy game from Bullfrog.
+/******************************************************************************/
+/** @file matrix.h
+ *     Header file for matrix.c.
+ * @par Purpose:
+ *     Routines implementing matrix support.
+ * @par Comment:
+ *     Just a header file - #defines, typedefs, function prototypes etc.
+ * @author   Tomasz Lis
+ * @date     27 May 2022 - 12 Jun 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "bftypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/******************************************************************************/
+#pragma pack(1)
+
+struct M33 {
+  int R[3][3];
+};
+
+#pragma pack()
+/******************************************************************************/
+
+/******************************************************************************/
+#ifdef __cplusplus
+}
+#endif
+#endif
