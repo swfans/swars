0b74ba4004655c342f910b54f8ca6b07b46475d6
mefistotelis <mefistotelis@gmail.com>
2023-08-05 15:24:22 +0200

    swars: Separate file bor buildings

---
+++ src/building.c
@@ -0,0 +1,24 @@
+/******************************************************************************/
+// Syndicate Wars Port, source port of the classic strategy game from Bullfrog.
+/******************************************************************************/
+/** @file building.c
+ *     Buildings related functions.
+ * @par Purpose:
+ *     Implement functions for handling buildings.
+ * @par Comment:
+ *     None.
+ * @author   Tomasz Lis
+ * @date     27 May 2022 - 12 Jun 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include "building.h"
+
+#include "bfmemory.h"
+/******************************************************************************/
+
+/******************************************************************************/
