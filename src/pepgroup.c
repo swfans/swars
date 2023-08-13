c4ed18a4cf648501b1a31edfe5eea114d4fbf8d1
mefistotelis <mefistotelis@gmail.com>
2023-08-14 01:56:35 +0200

    swars: Added file for people groups

---
+++ src/pepgroup.c
@@ -0,0 +1,25 @@
+/******************************************************************************/
+// Syndicate Wars Port, source port of the classic strategy game from Bullfrog.
+/******************************************************************************/
+/** @file pepgroup.c
+ *     People groups support.
+ * @par Purpose:
+ *     Implement handling parameters of groups to which people are assigned.
+ * @par Comment:
+ *     None.
+ * @author   Tomasz Lis
+ * @date     19 Apr 2023 - 11 Jul 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include "pepgroup.h"
+
+#include "bfutility.h"
+#include "bfmemut.h"
+/******************************************************************************/
+
+/******************************************************************************/
---
+++ src/pepgroup.h
@@ -0,0 +1,40 @@
+/******************************************************************************/
+// Syndicate Wars Port, source port of the classic strategy game from Bullfrog.
+/******************************************************************************/
+/** @file pepgroup.h
+ *     Header file for pepgroup.c.
+ * @par Purpose:
+ *     People groups support.
+ * @par Comment:
+ *     Just a header file - #defines, typedefs, function prototypes etc.
+ * @author   Tomasz Lis
+ * @date     19 Apr 2023 - 11 Jul 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef PEPGROUP_H
+#define PEPGROUP_H
+
+#include "bftypes.h"
+#include "cybmod.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/******************************************************************************/
+#pragma pack(1)
+
+#define PEOPLE_GROUPS_COUNT 32
+
+#pragma pack()
+/******************************************************************************/
+
+/******************************************************************************/
+#ifdef __cplusplus
+}
+#endif
+#endif
