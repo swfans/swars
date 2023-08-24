87c97de6272fc6b044f99baa534523cb389a69b9
mefistotelis <mefistotelis@gmail.com>
2023-08-24 16:52:10 +0200

    swars: Remade draw_objective()

--- src/lvobjctv.c
+++ src/lvobjctv.c
@@ -232,6 +232,7 @@ int add_used_objective(long mapno, long levelno)
 
 void draw_objective(ushort objectv, ubyte flag)
 {
+#if 0
     struct Objective *p_objectv;
     ushort bkpType;
     // workaround due to scanner not understanding new objectives
@@ -253,6 +254,171 @@ void draw_objective(ushort objectv, ubyte flag)
     asm volatile ("call ASM_draw_objective\n"
         : : "a" (objectv), "d" (flag));
     p_objectv->Type = bkpType;
+#else
+    struct Objective *p_objectv;
+    struct ObjectiveDef *p_odef;
+    struct Thing *p_thing;
+    short plyagent, plygroup;
+    short thing, group;
+    ubyte colk;
+
+    p_objectv = &game_used_objectives[objectv];
+    if (flag == 1)
+    {
+        dword_1C8464 = 0;
+        draw_objectv_x = 200;
+        draw_objectv_y = 10;
+        dword_1C8460 = 0;
+        return;
+    }
+
+    if (p_objectv->Type < sizeof(objectv_defs)/sizeof(objectv_defs[0]))
+        p_odef = &objectv_defs[p_objectv->Type];
+    else
+        p_odef = &objectv_defs[0];
+
+    if (!byte_1C844F)
+    {
+        if ((p_objectv->Flags & 0x01) != 0)
+            scroll_text = "-";
+        else if (p_objectv->ObjText != 0)
+            scroll_text = objective_text[p_objectv->ObjText];
+        else
+            scroll_text = p_odef->DefText;
+        ++dword_1C8464;
+
+    }
+    else
+    {
+        char locstr[112];
+        group = p_objectv->Thing;
+        if (group >= PEOPLE_GROUPS_COUNT) group = 0;
+        sprintf(locstr, "[%d] %20s t %d id %d", group_actions[group].Alive,
+          p_odef->CmdName, p_objectv->Thing, p_objectv->UniqueID);
+        if (gameturn & 4)
+            colk = dword_1C8460 & 7;
+        else
+            colk = 1;
+        draw_text(2 * draw_objectv_x, 2 * draw_objectv_y, locstr, colour_lookup[colk]);
+    }
+
+    if (!byte_1C844F) {
+        draw_objectv_y += 8;
+        dword_1C8460++;
+        return;
+    }
+
+    p_objectv = &game_used_objectives[objectv];
+    colk = dword_1C8460 & 7;
+
+    switch (p_objectv->Type)
+    {
+    case GAME_OBJ_PERSUADE_ALL_G:
+        group = p_objectv->Thing;
+        for (thing = same_type_head[256 + group]; thing != 0; thing = p_thing->LinkSameGroup)
+        {
+            p_thing = &things[thing];
+            if ((things[thing].Flag & 0x80000) == 0) {
+                draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+            }
+        }
+        break;
+    case GAME_OBJ_GET_ITEM:
+    case GAME_OBJ_DESTROY_OBJECT:
+        thing = p_objectv->Thing;
+        draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+        break;
+    case GAME_OBJ_TIME:
+    case GAME_OBJ_USE_ITEM:
+    case GAME_OBJ_FUNDS:
+    case GAME_OBJ_PKILL_P:
+        break;
+    case GAME_OBJ_PKILL_ALL_G:
+    case GAME_OBJ_PKILL_MEM_G:
+        group = p_objectv->Thing;
+        plyagent = players[local_player_no].DirectControl[0];
+        plygroup = things[plyagent].U.UPerson.Group;
+        for (thing = same_type_head[256 + group]; thing != 0; thing = p_thing->LinkSameGroup)
+        {
+            p_thing = &things[thing];
+            if (((p_thing->Flag & 0x80000) == 0) || things[p_thing->Owner].U.UPerson.Group != plygroup) {
+                if ((p_thing->Flag & 0x02) == 0)
+                    draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+            }
+        }
+        draw_objectv_y += 8;
+        break;
+    case GAME_OBJ_P_PERS_G:
+        group = p_objectv->Thing;
+        for (thing = same_type_head[256 + group]; thing != 0; thing = p_thing->LinkSameGroup)
+        {
+            p_thing = &things[thing];
+            if (((p_thing->Flag & 0x80000) == 0) && (p_thing->Owner != p_objectv->Arg2)) {
+                if ((p_thing->Flag & 0x02) == 0)
+                    draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+            }
+        }
+        break;
+    case GAME_OBJ_ALL_G_USE_V:
+    case GAME_OBJ_MEM_G_USE_V:
+        group = p_objectv->Arg2;
+        for (thing = same_type_head[256 + group]; thing != 0; thing = p_thing->LinkSameGroup)
+        {
+            p_thing = &things[thing];
+            draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+        }
+        draw_objectv_y += 8;
+        draw_objective_point(draw_objectv_x - 10, draw_objectv_y, p_objectv->Thing, 0, colour_lookup[colk+1]);
+        break;
+    case GAME_OBJ_DESTROY_V:
+        break;
+    case GAME_OBJ_PROTECT_G:
+    case GAME_OBJ_ALL_G_DEAD:
+    case GAME_OBJ_MEM_G_DEAD:
+        group = p_objectv->Thing;
+        for (thing = same_type_head[256 + group]; thing != 0; thing = p_thing->LinkSameGroup)
+        {
+            p_thing = &things[thing];
+            if ((p_thing->Flag & 0x02) == 0)
+                draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+        }
+        break;
+    case GAME_OBJ_P_NEAR:
+    case GAME_OBJ_USE_PANET:
+    case GAME_OBJ_UNUSED_21:
+        break;
+    case GAME_OBJ_P_DEAD:
+        draw_objective_point(draw_objectv_x - 10, draw_objectv_y, p_objectv->Thing, 0, colour_lookup[colk]);
+        break;
+    case GAME_OBJ_MEM_G_NEAR:
+    case GAME_OBJ_MEM_G_ARRIVES:
+    case GAME_OBJ_ALL_G_ARRIVES:
+        // Arrival place
+        func_711F4(p_objectv->X, p_objectv->Y, p_objectv->Z, p_objectv->Radius << 6, colour_lookup[2]);
+        unkn_draw_transformed_point(draw_objectv_x - 10, draw_objectv_y,
+          p_objectv->X, p_objectv->Y, p_objectv->Z, colour_lookup[colk]);
+        break;
+    case GAME_OBJ_P_ARRIVES:
+    case GAME_OBJ_V_ARRIVES:
+    case GAME_OBJ_ITEM_ARRIVES:
+        // Person / Vehicle / Item
+        thing = p_objectv->Thing;
+        draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+        // Arrival place
+        func_711F4(p_objectv->X, p_objectv->Y, p_objectv->Z, p_objectv->Radius << 6, colour_lookup[2]);
+        unkn_draw_transformed_point(draw_objectv_x - 10, draw_objectv_y,
+          p_objectv->X, p_objectv->Y, p_objectv->Z, colour_lookup[colk]);
+        break;
+    case GAME_OBJ_PERSUADE_P:
+        thing = p_objectv->Thing;
+        draw_objective_point(draw_objectv_x - 10, draw_objectv_y, thing, 0, colour_lookup[colk]);
+        break;
+    case GAME_OBJ_PERSUADE_MEM_G:
+        break;
+    }
+    draw_objectv_y += 8;
+    dword_1C8460++;
+#endif
 }
 
 TbBool thing_arrived_at_obj(short thing, struct Objective *p_objectv)
--- src/scandraw.c
+++ src/scandraw.c
@@ -22,6 +22,7 @@
 #include "engintrns.h"
 #include "campaign.h"
 #include "display.h"
+#include "thing.h"
 #include "swlog.h"
 /******************************************************************************/
 
@@ -34,6 +35,37 @@ void unkn_draw_transformed_point(short x, short y, long ptX, long ptY, long ptZ,
     ep.Y3d = ptY - engn_yc;
     transform_point(&ep);
     LbDrawLine(x, y, ep.pp.X, ep.pp.Y, colour);
+}
+
+void draw_objective_point(long x, long y, short thing, short a4, ubyte colour)
+{
+    long ptX, ptY, ptZ;
+
+    if (a4)
+        return;
+
+    if (thing <= 0) {
+        struct SimpleThing *p_sthing;
+        p_sthing = &sthings[thing];
+        ptX = p_sthing->X;
+        ptY = p_sthing->Y;
+        ptZ = p_sthing->Z;
+    } else {
+        struct Thing *p_thing;
+        p_thing = &things[thing];
+        ptX = p_thing->X;
+        ptY = p_thing->Y;
+        ptZ = p_thing->Z;
+    }
+    unkn_draw_transformed_point(x, y, ptX, ptY, ptZ, colour);
+}
+
+void func_711F4(short a1, short a2, short a3, short a4, ubyte colour)
+{
+    asm volatile (
+      "push %4\n"
+      "call ASM_func_711F4\n"
+        : : "a" (a1), "d" (a2), "b" (a3), "c" (a4), "g" (colour));
 }
 
 /******************************************************************************/
