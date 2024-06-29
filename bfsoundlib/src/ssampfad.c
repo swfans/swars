d883724289bac23643f2f4f5e1e263a9886f616d
mefistotelis <mefistotelis@gmail.com>
2024-06-29 23:05:47 +0200

    bfsoundlib: Remafde cbfadesample()
    Unused in SW, but part of the sound lib.

--- bfsoundlib/src/ssampfad.c
+++ bfsoundlib/src/ssampfad.c
@@ -27,6 +27,9 @@
 #include "bfsound.h"
 #include "aildebug.h"
 /******************************************************************************/
+extern TbBool SoundInstalled;
+extern TbBool SoundAble;
+extern TbBool SoundActive;
 
 extern struct SampleInfo sample_id[32];
 extern struct SampleInfo *end_sample_id;
@@ -35,8 +38,116 @@ extern long sample_fade_handle;
 
 extern ubyte volatile samples_currently_fading;
 extern ubyte volatile switch_off_sample_fade_timer;
+extern ubyte a_sample_is_fading; // = 0;
 
 /******************************************************************************/
+
+void cbfadesample()
+{
+    struct SampleInfo *p_smpinf;
+    int i;
+
+    if (!SoundInstalled || !SoundAble || !SoundActive)
+    {
+        switch_off_sample_fade_timer = 1;
+        samples_currently_fading = 0;
+        for (p_smpinf = sample_id; p_smpinf <= end_sample_id; p_smpinf++)
+        {
+            p_smpinf->FadeState = 0;
+            p_smpinf->FadeStopFlag = 0;
+        }
+        return;
+    }
+
+    if (switch_off_sample_fade_timer)
+    {
+        AIL_release_timer_handle(sample_fade_handle);
+        return;
+    }
+
+    a_sample_is_fading = switch_off_sample_fade_timer;
+
+    for (p_smpinf = sample_id; p_smpinf <= end_sample_id; p_smpinf++)
+    {
+        switch (p_smpinf->FadeState)
+        {
+        case 1:
+            a_sample_is_fading = 1;
+            if (AIL_sample_status(p_smpinf->SampleHandle) != SNDSMP_PLAYING)
+            {
+                p_smpinf->FadeState = 0;
+                p_smpinf->FadeStopFlag = 0;
+                break;
+            }
+            if (p_smpinf->FadeToVolume > 127)
+                p_smpinf->FadeToVolume = 127;
+            if (p_smpinf->FadeToVolume <= p_smpinf->SampleVolume)
+            {
+                p_smpinf->FadeState = 0;
+                if (p_smpinf->FadeStopFlag) {
+                    AIL_end_sample(p_smpinf->SampleHandle);
+                    p_smpinf->FadeStopFlag = 0;
+                }
+            }
+            if (p_smpinf->FadeState != 1)
+                break;
+            for (i = 1; i <= p_smpinf->FadeStep ; i++)
+            {
+                p_smpinf->SampleVolume++;
+                if (p_smpinf->SampleVolume >= 127) {
+                    p_smpinf->SampleVolume = 127;
+                    break; // for() only
+                }
+                if (p_smpinf->FadeToVolume <= p_smpinf->SampleVolume) {
+                    p_smpinf->SampleVolume = p_smpinf->FadeToVolume;
+                    break; // for() only
+                }
+            }
+            AIL_set_sample_volume(p_smpinf->SampleHandle, p_smpinf->SampleVolume);
+            break;
+        case 2:
+            a_sample_is_fading = 1;
+            if (AIL_sample_status(p_smpinf->SampleHandle) != SNDSMP_PLAYING)
+            {
+                p_smpinf->FadeState = 0;
+                p_smpinf->FadeStopFlag = 0;
+                break;
+            }
+            if (p_smpinf->FadeToVolume >= p_smpinf->SampleVolume)
+            {
+                p_smpinf->FadeState = 0;
+                if (p_smpinf->FadeStopFlag) {
+                    AIL_end_sample(p_smpinf->SampleHandle);
+                    p_smpinf->FadeStopFlag = 0;
+                }
+            }
+            if (p_smpinf->FadeState != 2)
+                break;
+            for (i = 1; i <= p_smpinf->FadeStep ; i++)
+            {
+                p_smpinf->SampleVolume--;
+                if (p_smpinf->SampleVolume <= 0) {
+                    p_smpinf->SampleVolume = 0;
+                    break; // for() only
+                }
+                if (p_smpinf->FadeToVolume >= p_smpinf->SampleVolume) {
+                    p_smpinf->SampleVolume = p_smpinf->FadeToVolume;
+                    break; // for() only
+                }
+            }
+            AIL_set_sample_volume(p_smpinf->SampleHandle, p_smpinf->SampleVolume);
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (!a_sample_is_fading)
+    {
+        switch_off_sample_fade_timer = 1;
+        samples_currently_fading = 0;
+    }
+}
 
 void StopAllSampleFadeTimers(void)
 {
