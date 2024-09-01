a91803a7f9b67fe47229873665aad8c7b1ecb062
mefistotelis <mefistotelis@gmail.com>
2024-09-01 21:32:36 +0200

    bflibrary: Add internal blitting to SDL2
    Since SDL2 has scaled blitting functionality, I was under impression
    this is no longer needed. But turns out the standard SDL2 blit funct
    just doesn't implement some of the fixel formats. So, adding the
    code from SDL1 back to SDL2.

--- bflibrary/src/x86-win-sdl2/sscreen.c
+++ bflibrary/src/x86-win-sdl2/sscreen.c
@@ -836,6 +836,137 @@ TbBool LbHwCheckIsModeAvailable(TbScreenMode mode)
     return firstSurfaceOk && secondSurfaceOk;
 }
 
+static void LbI_SDL_BlitScaled_to8bpp(long src_w, long src_h, ubyte *src_buf,
+  long dst_w, long dst_h, ubyte *dst_buf)
+{
+    // denominator of a (source) pixel's fraction part
+    const long denom_i = 2 * dst_h;
+    const long denom_j = 2 * dst_w;
+
+    // number of whole units in each (source) step
+    const long dsrc_i = 2 * src_h / denom_i;
+    const long dsrc_j = 2 * src_w / denom_j;
+
+    // numerator of fractional part of each (source) step
+    const long dsrc_num_i = (2 * src_h) - dsrc_i * denom_i;
+    const long dsrc_num_j = (2 * src_w) - dsrc_j * denom_j;
+
+    // number of whole units in a (source) half-step
+    const long halfdsrc_i = src_h / denom_i;
+    const long halfdsrc_j = src_w / denom_j;
+
+    long dst_offset = 0;
+    long src_offset = halfdsrc_i * src_w + halfdsrc_j;
+
+    // start at fractional part of each (source) half-step
+    long src_num_i =  src_h - halfdsrc_i * denom_i;
+    long src_num_j = src_w - halfdsrc_j * denom_j;
+
+    for (long i = 0; i != dst_h; ++i) {
+        if (src_num_i > denom_i) {
+            src_num_i -= denom_i;
+            src_offset += src_w;
+        }
+        for (long j = 0; j != dst_w; ++j) {
+            if (src_num_j > denom_j) {
+                src_num_j -= denom_j;
+                ++src_offset;
+            }
+            dst_buf[dst_offset] = src_buf[src_offset];
+            dst_offset++;
+            src_offset += dsrc_j;
+            src_num_j += dsrc_num_j;
+        }
+        src_offset += (dsrc_i - 1) * src_w;
+        src_num_i += dsrc_num_i;
+    }
+}
+
+static void LbI_SDL_BlitScaled_totcbpp(long src_w, long src_h, ubyte *src_buf,
+  SDL_Color *pal, long rshift, long gshift, long bshift,
+  long dst_w, long dst_h, long dst_bpp, ubyte *dst_buf)
+{
+    // denominator of a (source) pixel's fraction part
+    const long denom_i = 2 * dst_h;
+    const long denom_j = 2 * dst_w;
+
+    // number of whole units in each (source) step
+    const long dsrc_i = 2 * src_h / denom_i;
+    const long dsrc_j = 2 * src_w / denom_j;
+
+    // numerator of fractional part of each (source) step
+    const long dsrc_num_i = (2 * src_h) - dsrc_i * denom_i;
+    const long dsrc_num_j = (2 * src_w) - dsrc_j * denom_j;
+
+    // number of whole units in a (source) half-step
+    const long halfdsrc_i = src_h / denom_i;
+    const long halfdsrc_j = src_w / denom_j;
+
+    long dst_offset = 0;
+    long src_offset = halfdsrc_i * src_w + halfdsrc_j;
+
+    // start at fractional part of each (source) half-step
+    long src_num_i =  src_h - halfdsrc_i * denom_i;
+    long src_num_j = src_w - halfdsrc_j * denom_j;
+
+    for (long i = 0; i != dst_h; ++i) {
+        if (src_num_i > denom_i) {
+            src_num_i -= denom_i;
+            src_offset += src_w;
+        }
+        for (long j = 0; j != dst_w; ++j) {
+            SDL_Color c;
+            if (src_num_j > denom_j) {
+                src_num_j -= denom_j;
+                ++src_offset;
+            }
+            c = pal[src_buf[src_offset]];
+            *((long *)(dst_buf+dst_offset)) = (c.r << rshift) + (c.g << gshift) + (c.b << bshift);
+            dst_offset += dst_bpp;
+            src_offset += dsrc_j;
+            src_num_j += dsrc_num_j;
+        }
+        src_offset += (dsrc_i - 1) * src_w;
+        src_num_i += dsrc_num_i;
+    }
+}
+
+/** @internal
+ * Provides expanded SDL_BlitScaled() functionality with support of more format conversions.
+ */
+int LbI_SDL_BlitScaled(SDL_Surface *src, SDL_Surface *dst)
+{
+    long dst_bpp;
+
+    // shortcircuit for 1:1
+    if (src->w == dst->w && src->h == dst->h)
+        return SDL_BlitSurface(src, NULL, dst, NULL);
+
+    // the standard DSL2 blitting is better, for formats it actually supports
+    if (src->format->BytesPerPixel != 1)
+        return SDL_BlitScaled(src, NULL, dst, NULL);
+
+    if (SDL_MUSTLOCK(src) && SDL_LockSurface(src) < 0)
+        LOGERR("cannot lock source surface: %s", SDL_GetError());
+
+    if (SDL_MUSTLOCK(dst) && SDL_LockSurface(dst) < 0)
+        LOGERR("cannot lock destination Surface: %s", SDL_GetError());
+
+    dst_bpp = dst->format->BytesPerPixel;
+    if (dst_bpp == 1)
+        LbI_SDL_BlitScaled_to8bpp(src->w, src->h, src->pixels,
+          dst->w, dst->h, dst->pixels);
+    else
+        LbI_SDL_BlitScaled_totcbpp(src->w, src->h, src->pixels,
+          src->format->palette->colors, dst->format->Rshift, dst->format->Gshift, dst->format->Bshift,
+          dst->w, dst->h, dst_bpp, dst->pixels);
+
+    if (SDL_MUSTLOCK(dst)) SDL_UnlockSurface(dst);
+    if (SDL_MUSTLOCK(src)) SDL_UnlockSurface(src);
+
+    return 0;
+}
+
 /** Check if the lbDrawSurface is linked to WScreen buffer; fix if neccessary.
  *
  * This call is required to handle WScreen pointer changes by the app side.
@@ -893,8 +1024,8 @@ TbResult LbScreenSwap(void)
     // Put the data from Draw Surface onto Screen Surface
     if ((ret == Lb_SUCCESS) && (lbHasSecondSurface))
     {
-        blresult = SDL_BlitScaled(to_SDLSurf(lbDrawSurface), NULL,
-          to_SDLSurf(lbScreenSurface), NULL);
+        blresult = LbI_SDL_BlitScaled(to_SDLSurf(lbDrawSurface),
+          to_SDLSurf(lbScreenSurface));
         if (blresult < 0) {
             LOGERR("blit failed: %s", SDL_GetError());
             ret = Lb_FAIL;
@@ -929,8 +1060,8 @@ TbResult LbScreenSwapClear(TbPixel colour)
     // Put the data from Draw Surface onto Screen Surface
     if ((ret == Lb_SUCCESS) && (lbHasSecondSurface))
     {
-        blresult = SDL_BlitScaled(to_SDLSurf(lbDrawSurface), NULL,
-          to_SDLSurf(lbScreenSurface), NULL);
+        blresult = LbI_SDL_BlitScaled(to_SDLSurf(lbDrawSurface),
+          to_SDLSurf(lbScreenSurface));
         if (blresult < 0) {
             LOGERR("blit failed: %s", SDL_GetError());
             ret = Lb_FAIL;
@@ -983,8 +1114,8 @@ TbResult LbScreenSwapBox(ubyte *sourceBuf, long sourceX, long sourceY,
     {
         SDL_Rect clipRect = {destX, destY, width, height};
         SDL_SetClipRect(to_SDLSurf(lbScreenSurface), &clipRect);
-        blresult = SDL_BlitScaled(to_SDLSurf(lbDrawSurface), NULL,
-          to_SDLSurf(lbScreenSurface), NULL);
+        blresult = LbI_SDL_BlitScaled(to_SDLSurf(lbDrawSurface),
+          to_SDLSurf(lbScreenSurface));
         SDL_SetClipRect(to_SDLSurf(lbScreenSurface), NULL);
         if (blresult < 0) {
             LOGERR("blit failed: %s", SDL_GetError());
