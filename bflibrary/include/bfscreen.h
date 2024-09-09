8ed795e06fee462e621f494bcee5a30aead48950
mefistotelis <mefistotelis@gmail.com>
2024-09-09 10:51:05 +0200

    bflibrary: Define build params for more mouse control

--- bflibrary/include/bfscreen.h
+++ bflibrary/include/bfscreen.h
@@ -227,12 +227,12 @@ struct DisplayStruct { // sizeof=118
     /** Currently active colour palette.
      *  LbPaletteGet() should be used to retrieve a copy of the palette. */
     ubyte *Palette; // offset=114
-#if defined(ENABLE_MOUSE_WHEEL)
+#if defined(LB_ENABLE_MOUSE_WHEEL)
     short WheelPosition;
     ushort WheelMoveUp;
     ushort WheelMoveDown;
 #endif
-#if defined(ENABLE_MOUSE_MOVE_RATIO)
+#if defined(LB_ENABLE_MOUSE_MOVE_RATIO)
     short MouseMoveRatioX;
     short MouseMoveRatioY;
 #endif
