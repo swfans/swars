743e4fab19bdeca83ee8ec2e096b96a9502cab15
mefistotelis <mefistotelis@gmail.com>
2024-09-29 13:44:14 +0200

    swars: Preparations to rewrite SCANNER_draw_new_transparent_sub2()
    Defined sub-functions and globals used by that function.

--- src/scandraw.c
+++ src/scandraw.c
@@ -18,13 +18,50 @@
 /******************************************************************************/
 #include "scandraw.h"
 
+#include "bfanywnd.h"
+#include "bfgentab.h"
 #include "bfline.h"
+#include "bfmath.h"
+#include "bfpixel.h"
+
 #include "engintrns.h"
 #include "campaign.h"
 #include "display.h"
 #include "thing.h"
+#include "game_speed.h"
+#include "game.h"
+#include "weapon.h"
 #include "swlog.h"
 /******************************************************************************/
+
+struct scanstr1 {
+    long u;
+    long v;
+};
+
+struct scanstr2 {
+    long du;
+    long dv;
+};
+
+struct scanstr3 {
+    long u1;
+    long v1;
+    long u2;
+    long v2;
+};
+
+extern long dword_155340[32];
+extern struct scanstr1 SCANNER_bbpoint[255];
+extern long dword_1DBB64[];
+extern long dword_1DBB6C[512];
+extern ulong SCANNER_keep_arcs;
+extern ubyte SCANNER_colour[5];
+extern long scanner_blink; // = 1;
+
+extern struct scanstr3 SCANNER_arcpoint[20];
+extern TbPixel SCANNER_people_colours[15];
+extern ubyte byte_1C5C30[8];
 
 void unkn_draw_transformed_point(short x, short y, long ptX, long ptY, long ptZ, ubyte colour)
 {
@@ -69,12 +106,41 @@ void func_711F4(short a1, short a2, short a3, short a4, ubyte colour)
         : : "a" (a1), "d" (a2), "b" (a3), "c" (a4), "g" (colour));
 }
 
+void SCANNER_draw_mark_point5_blink3(int x, int y, TbPixel col)
+{
+    asm volatile (
+      "call ASM_SCANNER_draw_mark_point5_blink3\n"
+        : : "a" (x), "d" (y), "b" (col));
+}
+
+void SCANNER_draw_mark_point7(int x, int y, TbPixel col)
+{
+    asm volatile (
+      "call ASM_SCANNER_draw_mark_point7\n"
+        : : "a" (x), "d" (y), "b" (col));
+}
+
+void SCANNER_process_arcpoints(void)
+{
+    asm volatile (
+      "call ASM_SCANNER_process_arcpoints\n"
+        :  :  : "eax" );
+}
+
+void SCANNER_draw_new_transparent_sub2(void)
+{
+#if 1
+    asm volatile ("call ASM_SCANNER_draw_new_transparent_sub2\n"
+        :  :  : "eax" );
+    return;
+#endif
+}
+
 void SCANNER_draw_new_transparent(void)
 {
     asm volatile ("call ASM_SCANNER_draw_new_transparent_sub1\n"
         :  :  : "eax" );
-    asm volatile ("call ASM_SCANNER_draw_new_transparent_sub2\n"
-        :  :  : "eax" );
+    SCANNER_draw_new_transparent_sub2();
 }
 
 /******************************************************************************/
