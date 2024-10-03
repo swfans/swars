54da56caa1d533fdc80904640f9f3bfd90e651e7
mefistotelis <mefistotelis@gmail.com>
2024-10-03 12:29:29 +0200

    bflibrary: Moved poly_line() to correct place

--- bflibrary/src/general/poly.c
+++ bflibrary/src/general/poly.c
@@ -17,10 +17,13 @@
  *     (at your option) any later version.
  */
 /******************************************************************************/
+#include <assert.h>
 #include "poly.h"
+
 #include "poly_trigp.h"
 #include "poly_trigr.h"
 #include "bfgentab.h"
+#include "bfmemut.h"
 #include "bfpalette.h"
 #include "privbflog.h"
 
@@ -211,6 +214,222 @@ void trig(struct PolyPoint *point_a, struct PolyPoint *point_b,
     case RendVec_mode26:
         trig_render_md26(&tlr);
         break;
+    }
+
+    LOGNO("end");
+}
+
+void poly_line(struct PolyPoint *point_a, struct PolyPoint *point_b)
+{
+    TbPixel *obuf;
+    ushort i;
+    short x2, y2;
+    short x1, y1;
+    short chk_y;
+    short dt_x;
+
+    LOGNO("Pa(%ld,%ld,%ld)", point_a->X, point_a->Y, point_a->S);
+    LOGNO("Pb(%ld,%ld,%ld)", point_b->X, point_b->Y, point_b->S);
+
+    x1 = point_a->X;
+    y1 = point_a->Y;
+    x2 = point_b->X;
+    y2 = point_b->Y;
+
+    if (y1 < 0)
+    {
+        if (y2 < 0)
+            return;
+        chk_y = y2;
+        x1 += -y1 * (x2 - x1) / (y2 - y1);
+        y1 = 0;
+    }
+    else if (y2 < 0)
+    {
+        y2 = point_a->Y;
+        y1 = point_b->Y;
+        x1 = point_b->X;
+        x2 = point_a->X;
+
+        if (y2 < 0)
+            return;
+        chk_y = y2;
+        x1 += -y1 * (x2 - x1) / (y2 - y1);
+        y1 = 0;
+    }
+    else if (y1 == y2)
+    {
+        // Horizonal line optimization
+        ushort w;
+        short sx1, sx2;
+
+        if (y1 >= vec_window_height)
+            return;
+        sx1 = x1;
+        sx2 = x2;
+        if (x2 < x1)
+        {
+          sx2 = x1;
+          sx1 = x2;
+        }
+        if (sx1 >= 0)
+        {
+          if (sx1 > vec_window_width - 1)
+              return;
+        }
+        else
+        {
+          if (sx2 <= 0)
+              return;
+          sx1 = 0;
+        }
+        if (sx2 > vec_window_width - 1)
+            sx2 = vec_window_width - 1;
+        w = sx2 - sx1;
+        obuf = vec_screen + vec_screen_width * y1 + sx1;
+        LbMemorySet(obuf, vec_colour, w + 1);
+        return;
+    }
+    else if (y1 - y2 < 0)
+    {
+        if (y1 >= vec_window_height)
+            return;
+        chk_y = y2;
+    }
+    else
+    {
+        if (y2 >= vec_window_height)
+            return;
+        chk_y = y1;
+        y2 = point_a->Y;
+        y1 = point_b->Y;
+        x1 = point_b->X;
+        x2 = point_a->X;
+    }
+
+    if (chk_y >= vec_window_height) {
+        x2 = x1 + (vec_window_height - y1) * (x2 - x1) / (y2 - y1);
+        y2 = vec_window_height - 1;
+    }
+
+    if (x1 < 0)
+    {
+        if (x2 < 0)
+            return;
+        dt_x = 1;
+        y1 += -x1 * (y2 - y1) / (x2 - x1);
+        x1 = 0;
+        if (x2 >= vec_window_width) {
+            y2 = y1 + vec_window_width * (y2 - y1) / x2;
+            x2 = vec_window_width - 1;
+        }
+    }
+    else if (x1 >= vec_window_width)
+    {
+        if (x2 >= vec_window_width)
+            return;
+        dt_x = -1;
+        y1 += (x1 - vec_window_width) * (y2 - y1) / (x1 - x2);
+        x1 = vec_window_width - 1;
+        if (x2 < 0)
+        {
+            y2 -= -x2 * (y2 - y1) / (x1 - x2);
+            x2 = 0;
+        }
+    }
+    else if (x2 < 0)
+    {
+        dt_x = -1;
+        y2 = y1 + x1 * (y2 - y1) / (x1 - x2);
+        x2 = 0;
+    }
+    else if (x2 >= vec_window_width)
+    {
+        dt_x = 1;
+        y2 = y1 + (vec_window_width - x1) * (y2 - y1) / (x2 - x1);
+        x2 = vec_window_width - 1;
+    }
+    else if (x1 == x2)
+    {
+        // Vertical line optimization
+        ushort h;
+        short sy1;
+
+        if (y2 >= y1) {
+            sy1 = y1;
+            h = y2 - y1;
+        } else {
+            sy1 = y2;
+            h = y1 - y2;
+        }
+        obuf = vec_screen + vec_screen_width * sy1 + x1;
+        for (i = h + 1; i != 0; i--)
+        {
+            *obuf = vec_colour;
+            obuf += vec_screen_width;
+        }
+        return;
+    }
+    else if (x1 - x2 < 0)
+    {
+        dt_x = 1;
+    }
+    else
+    {
+        dt_x = -1;
+    }
+
+    if (y2 == y1)
+    {
+        // Horizonal line optimization
+        ushort w;
+        short sx1;
+
+        if (x2 >= x1) {
+            sx1 = x1;
+            w = x2 - x1;
+        } else {
+            sx1 = x2;
+            w = x1 - x2;
+        }
+        obuf = vec_screen + vec_screen_width * y1 + sx1;
+        LbMemorySet(obuf, vec_colour, w + 1);
+        return;
+    }
+
+    {
+        ushort dist_x, dist_y;
+        short sh_posit, sh_negat, sh_chk;
+        int dt_buf_x, dt_buf_y;
+
+        obuf = vec_screen + vec_screen_width * y1 + x1;
+        dist_x = dt_x * (x2 - x1);
+        dist_y = y2 - y1;
+        if (dist_y <= dist_x) {
+            dt_buf_x = dt_x;
+            dt_buf_y = vec_screen_width;
+        } else {
+            dist_x = y2 - y1;
+            dist_y = dt_x * (x2 - x1);
+            dt_buf_x = vec_screen_width;
+            dt_buf_y = dt_x;
+        }
+        sh_negat = 2 * dist_y;
+        sh_chk = 2 * dist_y - dist_x;
+        sh_posit = 2 * (dist_y - dist_x);
+        *obuf = vec_colour;
+        for (i = dist_x; i != 0; i--)
+        {
+            obuf += dt_buf_x;
+            if (sh_chk >= 0) {
+                obuf += dt_buf_y;
+                sh_chk += sh_posit;
+            } else {
+                sh_chk += sh_negat;
+            }
+            assert(obuf < vec_screen + vec_screen_width * vec_window_height);
+            *obuf = vec_colour;
+        }
     }
 
     LOGNO("end");
