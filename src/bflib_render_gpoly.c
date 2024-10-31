af2d6c77afc5deb7383fe1e4710d8880ae03d841
mefistotelis <mefistotelis@gmail.com>
2024-10-31 21:01:42 +0100

    swars: Remove draw_gpoly() as it is now part of bflibrary

--- src/bflib_render_gpoly.c
+++ src/bflib_render_gpoly.c
@@ -31,9 +31,6 @@
 
 long gpoly_pro_enable_mode_ofs;
 
-// Static variables used only inside draw_gpoly().
-long gpoly_mode;
-
 /******************************************************************************/
 void gpoly_enable_pentium_pro(TbBool state)
 {
@@ -55,93 +52,6 @@ void draw_trigpoly(struct PolyPoint *point_a, struct PolyPoint *point_b, struct 
     default:
       trig(point_a, point_b, point_c);
       break;
-    }
-}
-
-void draw_gpoly(struct PolyPoint *point_a, struct PolyPoint *point_b, struct PolyPoint *point_c)
-{
-#if 0
-    asm volatile (
-      "call ASM_draw_gpoly_\n"
-        : : "a" (point_a), "d" (point_b), "b" (point_c));
-    return;
-#endif
-    struct gpoly_state st;
-    ubyte start_type, cross_bound;
-
-    gpoly_mode = vec_mode;
-
-    cross_bound = (point_c->X | point_b->X | point_a->X) < 0
-             || point_a->X > vec_window_width
-             || point_b->X > vec_window_width
-             || point_c->X > vec_window_width;
-
-
-    start_type = gpoly_reorder_input_points(&point_a, &point_b, &point_c);
-    if (start_type == RendStart_NO)
-        return;
-
-    gpoly_init_state(&st, point_a, point_b, point_c);
-
-    switch (gpoly_mode)
-    {
-    case RendVec_mode02:
-    case RendVec_mode03:
-    case RendVec_mode12:
-    case RendVec_mode13:
-    case RendVec_mode18:
-    case RendVec_mode19:
-    case RendVec_mode22:
-    case RendVec_mode23:
-        gpoly_sta_md03(&st);
-        break;
-    case RendVec_mode04:
-    case RendVec_mode16:
-    case RendVec_mode17:
-        gpoly_sta_md04(&st);
-        break;
-    case RendVec_mode05:
-    case RendVec_mode06:
-    case RendVec_mode20:
-    case RendVec_mode21:
-    case RendVec_mode24:
-    case RendVec_mode25:
-        gpoly_sta_md05(&st);
-        break;
-    case 0x1Bu:
-    case 0x1Du:
-    case 0x1Fu:
-    case 0x20u:
-    case 0x21u:
-    case 0x22u:
-    case 0x25u:
-    case 0x26u:
-        gpoly_sta_md27(&st);
-        break;
-    case 0x1Cu:
-    case 0x1Eu:
-    case 0x23u:
-    case 0x24u:
-    case 0x27u:
-    case 0x28u:
-        gpoly_sta_md28(&st);
-        break;
-    }
-
-    switch (gpoly_mode)
-    {
-    case RendVec_mode05:
-        if (cross_bound)
-            gpoly_stb_md05uni_var040_nz(&st);
-        else
-            gpoly_stb_md05uni_var040_zr(&st);
-        break;
-    case 27:
-        if (cross_bound)
-            gpoly_stb_md05p64_var040_nz(&st);
-        else
-            gpoly_stb_md05p64_var040_zr(&st);
-        break;
     }
 }
 
