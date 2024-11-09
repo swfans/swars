023e79c87514754f0f55382beb7a7229bc5f17f2
mefistotelis <mefistotelis@gmail.com>
2024-11-09 20:37:20 +0100

    swars: Remade draw_pers_e_graphic()

--- src/engindrwlstm.c
+++ src/engindrwlstm.c
@@ -30,6 +30,12 @@
 #include "swlog.h"
 #include "thing.h"
 /******************************************************************************/
+extern ubyte byte_176D49;
+
+ubyte byte_152EF0[] = {
+   0, 10,  5, 10,  7,  7,  8, 10,
+  10, 10,  5,  7,  5,  7,  7,  0,
+};
 
 /** Add a new draw item and return linked SortLine instance.
  *
@@ -219,8 +225,8 @@ void draw_e_graphic(int x, int y, int z, ushort frame, int radius, int intensity
     return;
 #endif
     struct SortSprite *p_sspr;
-    int scr_dx, scr_dy, scr_z;
-    short scr_x, scr_y;
+    int scr_dx, scr_dy;
+    short scr_x, scr_y, scr_z;
     int fctr_xz, fctr_y;
     int abs_scr_dy, abs_scr_dx;
     int pers5_range;
@@ -289,8 +295,8 @@ void draw_e_graphic_scale(int x, int y, int z, ushort frame, int radius, int int
     return;
 #endif
     struct SortSprite *p_sspr;
-    int scr_dx, scr_dy, scr_z;
-    short scr_x, scr_y;
+    int scr_dx, scr_dy;
+    short scr_x, scr_y, scr_z;
     int fctr_xz, fctr_y;
     int abs_scr_dy, abs_scr_dx;
     int pers5_range;
@@ -346,12 +352,98 @@ void draw_e_graphic_scale(int x, int y, int z, ushort frame, int radius, int int
 
 void draw_pers_e_graphic(struct Thing *p_thing, int x, int y, int z, int frame, int radius, int intensity)
 {
+#if 0
     asm volatile (
       "push %6\n"
       "push %5\n"
       "push %4\n"
       "call ASM_draw_pers_e_graphic\n"
         : : "a" (p_thing), "d" (x), "b" (y), "c" (z), "g" (frame), "g" (radius), "g" (intensity));
+    return;
+#endif
+    struct SortSprite *p_sspr;
+    int scr_dx, scr_dy;
+    short scr_x, scr_y;
+    int fctr_xz, fctr_y;
+    int abs_scr_dy, abs_scr_dx;
+    int pers5_range;
+    int bckt_shift;
+    ubyte bri;
+
+    bri = byte_152EF0[p_thing->SubType] + intensity;
+
+    if (current_map == 9) // map009 Singapore on-water map
+        y += waft_table[gameturn & 0x1F] >> 3;
+
+    fctr_y = 8 * y - 8 * engn_yc;
+    fctr_xz = (dword_176D10 * x + dword_176D14 * z) >> 16;
+    pers5_range = (dword_176D18 * fctr_y + dword_176D1C * fctr_xz) >> 16;
+    abs_scr_dy = (dword_176D1C * fctr_y - dword_176D18 * fctr_xz) >> 16;
+    abs_scr_dx = (dword_176D14 * x - dword_176D10 * z) >> 16;
+
+    scr_dx = (overall_scale * abs_scr_dx) >> 11;
+    if (game_perspective == 5)
+        scr_dx = (scr_dx * (0x4000 - pers5_range)) >> 14;
+
+    scr_x = dword_176D3C + scr_dx;
+    if (scr_x < 0) {
+        if (scr_x < -2000)
+            scr_x = -2000;
+    } else if (scr_x >= vec_window_width) {
+        if (scr_x > 2000)
+            scr_x = 2000;
+    }
+
+    scr_dy = (overall_scale * abs_scr_dy) >> 11;
+    if (game_perspective == 5)
+        scr_dy = (scr_dy * (0x4000 - pers5_range)) >> 14;
+
+    scr_y = dword_176D40 - scr_dy;
+    if (scr_y < 0) {
+        if (scr_y < -2000)
+            scr_y = -2000;
+    } else if (scr_y >= vec_window_height) {
+        if (scr_y > 2000)
+            scr_y = 2000;
+    }
+
+    bckt_shift = pers5_range - radius;
+    if (ingame.DisplayMode == 50)
+    {
+        if ((p_thing->Flag2 & 0x20000000) != 0) {
+            if ((p_thing->Flag & 0x02) != 0)
+                return;
+            bckt_shift -= 1000000;
+        }
+    }
+    else
+    {
+        if ((p_thing->Flag2 & 0x20000000) != 0)
+            bckt_shift = -10000;
+    }
+
+    p_sspr = draw_item_add_sprite(DrIT_Unkn13, bckt_shift + 5000);
+    if (p_sspr == NULL)
+        return;
+
+    p_sspr->X = scr_x;
+    p_sspr->Y = scr_y;
+    p_sspr->Z = 0;
+    p_sspr->Frame = frame;
+    p_sspr->PThing = p_thing;
+    p_sspr->Brightness = bri;
+    p_sspr->Angle = (p_thing->U.UObject.Angle + 8 - byte_176D49) & 7;
+
+    if ((p_thing->Flag2 & 0x20000000) != 0 || p_thing->U.UPerson.OnFace || (p_thing->SubType == SubTT_PERS_MECH_SPIDER))
+        return;
+
+    p_sspr = draw_item_add_sprite(DrIT_Unkn19, bckt_shift + 5000 - 200);
+    if (p_sspr == NULL)
+        return;
+
+    p_sspr->X = scr_x;
+    p_sspr->Y = scr_y;
+    p_sspr->PThing = p_thing;
 }
 
 void FIRE_draw_fire(struct SimpleThing *p_sthing)
