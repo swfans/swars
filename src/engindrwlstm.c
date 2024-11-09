f10160d0ffb693569e4b1b7dfb12e058d09b26e7
mefistotelis <mefistotelis@gmail.com>
2024-11-09 18:52:14 +0100

    swars: Remade draw_e_graphic_scale()

--- src/engindrwlstm.c
+++ src/engindrwlstm.c
@@ -273,17 +273,75 @@ void draw_e_graphic(int x, int y, int z, ushort frame, int radius, int intensity
     p_sspr->Z = scr_z;
     p_sspr->Frame = frame;
     p_sspr->Brightness = intensity;
+    p_sspr->Scale = 256;
     p_sspr->PThing = p_thing;
 }
 
 void draw_e_graphic_scale(int x, int y, int z, ushort frame, int radius, int intensity, int scale)
 {
+#if 0
     asm volatile (
       "push %6\n"
       "push %5\n"
       "push %4\n"
       "call ASM_draw_e_graphic_scale\n"
         : : "a" (x), "d" (y), "b" (z), "c" (frame), "g" (radius), "g" (intensity), "g" (scale));
+    return;
+#endif
+    struct SortSprite *p_sspr;
+    int scr_dx, scr_dy, scr_z;
+    short scr_x, scr_y;
+    int fctr_xz, fctr_y;
+    int abs_scr_dy, abs_scr_dx;
+    int pers5_range;
+
+    if (current_map == 9) // map009 Singapore on-water map
+        y += waft_table[gameturn & 0x1F] >> 3;
+
+    fctr_y = 8 * y - 8 * engn_yc;
+    fctr_xz = (dword_176D10 * x + dword_176D14 * z) >> 16;
+    pers5_range = (dword_176D1C * fctr_xz + dword_176D18 * fctr_y) >> 16;
+    abs_scr_dy = (dword_176D1C * fctr_y - fctr_xz * dword_176D18) >> 16;
+    abs_scr_dx = (x * dword_176D14 - z * dword_176D10) >> 16;
+
+    scr_dx = (overall_scale * abs_scr_dx) >> 11;
+    if (game_perspective == 5)
+        scr_dx = (scr_dx * (0x4000 - pers5_range)) >> 14;
+
+    scr_x = scr_dx + dword_176D3C;
+    if (scr_x < 0) {
+        if (scr_x < -2000)
+            scr_x = -2000;
+    } else if (scr_x >= vec_window_width) {
+        if (scr_x > 2000)
+            scr_x = 2000;
+    }
+
+    scr_dy = (overall_scale * abs_scr_dy) >> 11;
+    if (game_perspective == 5)
+        scr_dy = (scr_dy * (0x4000 - pers5_range)) >> 14;
+
+    scr_y = dword_176D40 - scr_dy;
+    if (scr_y < 0) {
+        if (scr_y < -2000)
+            scr_y = -2000;
+    } else if (scr_y >= vec_window_height) {
+        if (scr_y > 2000)
+            scr_y = 2000;
+    }
+    scr_z = pers5_range - (unsigned __int16)radius - 100;
+
+    p_sspr = draw_item_add_sprite(DrIT_Unkn15, scr_z + 5000);
+    if (p_sspr == NULL)
+        return;
+
+    p_sspr->X = scr_x;
+    p_sspr->Y = scr_y;
+    p_sspr->Z = scr_z;
+    p_sspr->Frame = frame;
+    p_sspr->Brightness = intensity;
+    p_sspr->Scale = scale;
+    p_sspr->PThing = NULL;
 }
 
 void draw_pers_e_graphic(struct Thing *p_thing, int x, int y, int z, int frame, int radius, int intensity)
