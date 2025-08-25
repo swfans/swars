8d3fcbdcd23a54ed9b28289433440054b85b6cdf
mefistotelis <mefistotelis@gmail.com>
2025-08-26 00:25:34 +0200

    swars: Routed BAT_draw_char() through C

--- src/bat.c
+++ src/bat.c
@@ -18,8 +18,37 @@
 /******************************************************************************/
 #include "bat.h"
 
+#include <string.h>
+#include "bfkeybd.h"
+#include "bfmemut.h"
+#include "bfscreen.h"
+
+#include "display.h"
 #include "swlog.h"
 /******************************************************************************/
+
+void BAT_draw_char(int x, int y, char chr, ubyte col)
+{
+    asm volatile (
+      "call ASM_BAT_draw_char\n"
+        : : "a" (x), "d" (y), "b" (chr), "c" (col));
+}
+
+void BAT_print(short pos_x, short pos_y, const char *str, ubyte coll)
+{
+    char chr;
+    ubyte colour;
+
+    colour = colour_lookup[coll];
+    chr = *str;
+    while (chr != '\0')
+    {
+        if (chr != ' ')
+            BAT_draw_char(pos_x, pos_y, chr, colour);
+        chr = *++str;
+        pos_x += 4;
+    }
+}
 
 void BAT_play(void)
 {
